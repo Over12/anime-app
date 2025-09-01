bd5dadfbc595d93a3b7b13a9c127e9b12e3eac92
OverCode <avalosfigueroapedro@gmail.com>
2025-09-01 16:01:45 -0500

    feat: añadir componente Footer para mejorar la presentación de la aplicación

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "@/styles/globals.css";
 import { onest } from '@/styles/fonts'
 import NavBar from '@/components/ui/NavBar'
+import Footer from '@/components/ui/Footer'
 
 export const metadata: Metadata = {
   title: "Anime App",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className={onest.className}>
         <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/ui/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className='text-center spacing-x py-10'>
+      <span className='text-sm sm:text-base'>© 2025 hecho con ❤️ por <a href='https://overcode.netlify.app/' target='_blank' rel='noopener noreferrer' className='bg-clip-text bg-gradient-to-r from-[#748ca8] to-[#6c58c0] text-transparent font-bold'>Overcode</a></span>
+    </footer>
+  )
+}
