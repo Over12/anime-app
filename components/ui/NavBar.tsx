c431de0a85d25c266d9d8e5d6c110cfd12cb3d6f
OverCode <avalosfigueroapedro@gmail.com>
2025-09-03 17:34:33 -0500

    fix: corregir posición del navbar fijando la parte superior de la pantalla

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -6,7 +6,7 @@ import NavBarMobile from '../common/NavBar/NavBarMobile'
 export default function NavBar() {
   return (
     <>
-      <nav className='hidden fixed inset-x-0 bg-background/80 backdrop-blur-xl sm:grid grid-cols-3 spacing-x py-5 z-50 transition-colors'>
+      <nav className='hidden fixed top-0 inset-x-0 bg-background/80 backdrop-blur-xl sm:grid grid-cols-3 spacing-x py-5 z-50 transition-colors'>
         <span className='text-2xl font-bold justify-self-start'>Anime App</span>
         <NavigationLinks />
         <ButtonTheme />
