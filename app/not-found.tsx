522bc44fab26e675509023bbb4846ebb11db152f
OverCode <avalosfigueroapedro@gmail.com>
2025-09-03 17:34:25 -0500

    feat: añadir página NotFound con mensaje y enlace de retorno

---
+++ app/not-found.tsx
@@ -0,0 +1,16 @@
+export default function NotFound() {
+  return (
+    <section className="flex flex-col h-svh justify-center items-center text-center px-4">
+      <h1 className="text-6xl font-bold">404</h1>
+      <p className="mt-2 text-base sm:text-lg text-muted-foreground">
+        This page is missing... just like your favorite anime&apos;s next season.
+      </p>
+      <p className="mt-1 text-xs sm:text-sm text-muted-foreground italic">
+        (Is this canon... or just filler?)
+      </p>
+      <a href="/" className="mt-6 px-4 py-2 bg-primary text-white rounded-lg shadow-md hover:bg-primary/80 transition">
+        Take me back
+      </a>
+    </section>
+  )
+}
