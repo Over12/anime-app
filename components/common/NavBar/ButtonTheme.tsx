ea4fd14ca2fdf7a537149d171250a32a5afca599
OverCode <avalosfigueroapedro@gmail.com>
2025-09-04 17:46:25 -0500

    feat: añadir soporte para el modo 'system' en el botón de cambio de tema

--- components/common/NavBar/ButtonTheme.tsx
+++ components/common/NavBar/ButtonTheme.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { IconLoader, IconMoon, IconSun } from '@tabler/icons-react'
+import { IconLoader, IconMoon, IconSun, IconSunMoon } from '@tabler/icons-react'
 import { useTheme } from 'next-themes'
 import { motion } from 'motion/react'
 import { useEffect, useState } from 'react'
@@ -22,13 +22,28 @@ export default function ButtonTheme() {
   }
 
   const handleThemeChange = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light')
+    switch (theme) {
+      case 'light':
+        setTheme('dark')
+        break
+      case 'dark':
+        setTheme('system')
+        break
+      case 'system':
+        setTheme('light')
+        break
+      default:
+        setTheme('light')
+        break
+    }
   }
 
   return (
     <motion.button initial={{ scale: 0.8, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} transition={{ type: 'spring', stiffness: 300 }} whileTap={{ rotate: 90 }} className='justify-self-end cursor-pointer focus:outline-none' onClick={handleThemeChange}>
-      {theme === 'light' && <IconSun className='size-7' />}
-      {theme === 'dark' && <IconMoon className='size-7' />}
+      {theme === 'light' && <IconSun className='size-7' title='Light Mode' />}
+      {theme === 'dark' && <IconMoon className='size-7' title='Dark Mode' />}
+      {theme === 'system' && <IconSunMoon className='size-7' title='System Mode' />}
+      {!theme && <IconLoader className='size-7 animate-spin' title='Loading...' />}
     </motion.button>
   )
 }
