dba41e28cf9488a41209df014bfdafb6e38719cf
OverCode <avalosfigueroapedro@gmail.com>
2025-09-05 17:46:38 -0500

    refactor: reorganizar el formulario de búsqueda para mejorar la usabilidad y la estructura visual

--- components/common/Search/FormSearch.tsx
+++ components/common/Search/FormSearch.tsx
@@ -34,53 +34,57 @@ export default function FormSearch() {
   }
 
   return (
-    <form onSubmit={onSubmit} className='mb-5 flex items-center gap-5'>
+    <form onSubmit={onSubmit} className='mb-5 flex flex-col sm:flex-row items-center gap-5'>
       {pathname === '/animes' && (
         <div>
           <input type='text' name='q' placeholder='Search for an anime...' className='focus:outline-none focus:border-accent px-2 py-1 border-b border-primary' />
         </div>
       )}
-      <div>
-        <label htmlFor={pathname === '/animes' ? 'type' : 'filter'}>Type:</label>
-        <select name={pathname === '/animes' ? 'type' : 'filter'} id={pathname === '/animes' ? 'type' : 'filter'} className='focus:outline-none focus:border-accent px-2 py-1 bg-background border-b border-primary cursor-pointer ml-2 transition-colors'>
-          <option value=''>All</option>
-          <option value='tv'>TV</option>
-          <option value='movie'>Movie</option>
-          <option value='ova'>OVA</option>
-          <option value='special'>Special</option>
-          <option value='ona'>ONA</option>
-          <option value='music'>Music</option>
-          {pathname === '/animes' && (
-            <>
-              <option value='cm'>CM</option>
-              <option value='pv'>PV</option>
-              <option value='tv_special'>TV Special</option>
-            </>
-          )}
-        </select>
-      </div>
-      {pathname === '/animes' && (
+      <div className='flex items-center gap-3'>
         <div>
-          <label htmlFor='rating'>Rating:</label>
-          <select name='rating' id='rating' className='focus:outline-none focus:border-accent px-2 py-1 bg-background border-b border-primary cursor-pointer ml-2 transition-colors'>
+          <label htmlFor={pathname === '/animes' ? 'type' : 'filter'}>Type:</label>
+          <select name={pathname === '/animes' ? 'type' : 'filter'} id={pathname === '/animes' ? 'type' : 'filter'} className='focus:outline-none focus:border-accent px-2 py-1 bg-background border-b border-primary cursor-pointer ml-2 transition-colors'>
             <option value=''>All</option>
-            <option value='g'>G</option>
-            <option value='pg'>PG</option>
-            <option value='pg13'>PG-13</option>
-            <option value='r17'>R-17+</option>
-            <option value='r'>R+</option>
-            <option value='rx'>Rx</option>
+            <option value='tv'>TV</option>
+            <option value='movie'>Movie</option>
+            <option value='ova'>OVA</option>
+            <option value='special'>Special</option>
+            <option value='ona'>ONA</option>
+            <option value='music'>Music</option>
+            {pathname === '/animes' && (
+              <>
+                <option value='cm'>CM</option>
+                <option value='pv'>PV</option>
+                <option value='tv_special'>TV Special</option>
+              </>
+            )}
           </select>
         </div>
-      )}
-      <div className='flex items-center gap-2'>
-        <label htmlFor='sfw'>Safe for Work:</label>
-        <input type='checkbox' name='sfw' id='sfw' className='accent-primary size-4 focus:outline-none cursor-pointer' />
+        {pathname === '/animes' && (
+          <div>
+            <label htmlFor='rating'>Rating:</label>
+            <select name='rating' id='rating' className='focus:outline-none focus:border-accent px-2 py-1 bg-background border-b border-primary cursor-pointer ml-2 transition-colors'>
+              <option value=''>All</option>
+              <option value='g'>G</option>
+              <option value='pg'>PG</option>
+              <option value='pg13'>PG-13</option>
+              <option value='r17'>R-17+</option>
+              <option value='r'>R+</option>
+              <option value='rx'>Rx</option>
+            </select>
+          </div>
+        )}
       </div>
-      <button type='submit' className='flex items-center gap-1 px-3 py-2 text-white bg-primary rounded-md cursor-pointer hover:bg-accent transition-colors'>
-        <IconSearch className='size-5' />
-        Search
-      </button>
+      <div className='flex items-center gap-3'>
+        <div className='flex items-center gap-2'>
+          <label htmlFor='sfw'>Safe for Work:</label>
+          <input type='checkbox' name='sfw' id='sfw' className='accent-primary size-4 focus:outline-none cursor-pointer' />
+        </div>
+        <button type='submit' className='flex items-center gap-1 px-3 py-2 text-white bg-primary rounded-md cursor-pointer hover:bg-accent transition-colors'>
+          <IconSearch className='size-5' />
+          Search
+        </button>
+      </div>
     </form>
   )
 }
