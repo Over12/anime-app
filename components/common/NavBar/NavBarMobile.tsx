a42011694d17bb42fd0ae3ba40cfd99e1d921e68
OverCode <avalosfigueroapedro@gmail.com>
2025-09-05 17:45:58 -0500

    feat: añadir función de alternar menú y permitir cierre al hacer clic en los enlaces de navegación

--- components/common/NavBar/NavBarMobile.tsx
+++ components/common/NavBar/NavBarMobile.tsx
@@ -9,19 +9,23 @@ import NavigationLinks from './NavigationLinks'
 export default function NavBarMobile() {
   const [isOpen, setIsOpen] = useState<boolean>(false)
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <>
       <nav className='flex items-center justify-between sm:hidden fixed bg-background/80 backdrop-blur-xl inset-x-0 spacing-x py-5 z-50'>
         <span className='text-2xl font-bold'>Anime App</span>
-        <button className='focus:outline-none' onClick={() => setIsOpen(!isOpen)}>
+        <button className='focus:outline-none' onClick={toggleMenu}>
           <IconMenuDeep className='size-8' />
         </button>
         <Divisor />
       </nav>
       <AnimatePresence>
         {isOpen && (
-          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className='fixed inset-0 flex items-center justify-center bg-background z-40'>
-            <NavigationLinks />
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className='fixed inset-0 sm:hidden flex items-center justify-center bg-background z-40'>
+            <NavigationLinks toggleMenu={toggleMenu} />
           </motion.div>
         )}
       </AnimatePresence>
--- components/common/NavBar/NavigationLinks.tsx
+++ components/common/NavBar/NavigationLinks.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link'
 
-export default function NavigationLinks() {
+export default function NavigationLinks({ toggleMenu }: { toggleMenu?: () => void }) {
   return (
     <ul className='flex flex-col sm:flex-row justify-center items-center gap-7 sm:gap-10'>
-      <li><Link href="/" className='text-xl font-semibold sm:text-base sm:font-normal'>Home</Link></li>
-      <li><Link href="/animes" className='text-xl font-semibold sm:text-base sm:font-normal'>Animes</Link></li>
-      <li><Link href="/ongoing" className='text-xl font-semibold sm:text-base sm:font-normal'>Ongoing</Link></li>
-      <li><Link href="/upcoming" className='text-xl font-semibold sm:text-base sm:font-normal'>Upcoming</Link></li>
+      <li><Link onClick={toggleMenu} href="/" className='text-xl font-semibold sm:text-base sm:font-normal'>Home</Link></li>
+      <li><Link onClick={toggleMenu} href="/animes" className='text-xl font-semibold sm:text-base sm:font-normal'>Animes</Link></li>
+      <li><Link onClick={toggleMenu} href="/ongoing" className='text-xl font-semibold sm:text-base sm:font-normal'>Ongoing</Link></li>
+      <li><Link onClick={toggleMenu} href="/upcoming" className='text-xl font-semibold sm:text-base sm:font-normal'>Upcoming</Link></li>
     </ul>
   )
 }
