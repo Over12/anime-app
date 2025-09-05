7674dccd7884b0fa48adfeb89317ef1f28b87bcd
OverCode <avalosfigueroapedro@gmail.com>
2025-09-04 19:53:49 -0500

    refactor: eliminar AnimatePresence y optimizar el componente SkeletonAnimeCard

--- components/common/AnimeCard.tsx
+++ components/common/AnimeCard.tsx
@@ -2,7 +2,7 @@
 
 import { Anime } from '@/types/Anime'
 import Link from 'next/link'
-import { AnimatePresence, motion } from 'motion/react'
+import { motion } from 'motion/react'
 import { useState } from 'react'
 import SkeletonAnimeCard from './Skeleton/SkeletonAnimeCard'
 
@@ -11,9 +11,7 @@ export default function AnimeCard({ anime } : { anime: Anime }) {
 
   return (
     <div className='relative flex-shrink-0 max-w-40 sm:max-w-64 snap-start group'>
-      <AnimatePresence>
-        {isLoading && <SkeletonAnimeCard />}
-      </AnimatePresence>
+      {isLoading && <SkeletonAnimeCard />}
       <Link href={`/anime/${anime.mal_id}`}>
         <motion.img initial={{ opacity: 0, scale: 0.8 }} animate={{ opacity: 1, scale: 1 }} ref={(img) => { if (img && img.complete) setIsLoading(false) }} src={anime.images.webp.large_image_url} alt={anime.title} onLoad={() => setIsLoading(false)} loading='lazy' className={`w-40 sm:w-64 h-auto aspect-[2/3] rounded-md mb-1 shadow-xl ${isLoading ? 'opacity-0 pointer-events-none' : 'opacity-100'}`} />
         <span className='line-clamp-2 text-pretty font-semibold group-hover:text-primary transition-colors' title={anime.title}>{anime.title}</span>
