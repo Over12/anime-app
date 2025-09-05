bcd8c42a1726f509b4631bd59f2fb8d4cb65cb77
OverCode <avalosfigueroapedro@gmail.com>
2025-09-05 17:12:57 -0500

    fix: corregir la lógica de renderizado para evitar duplicados en la lista de animes

--- app/animes/page.tsx
+++ app/animes/page.tsx
@@ -1,16 +1,18 @@
 import AnimeCard from '@/components/common/AnimeCard'
 import Pagination from '@/components/common/Search/Pagination'
 import { fetchAnimes } from '@/lib/api'
+import { deleteDuplicate } from '@/lib/utils'
 import { AnimeQueryParameters } from '@/types/AnimeQueryParameters'
 
 export default async function page({ searchParams }: { searchParams: Promise<AnimeQueryParameters> }) {
   const params = await searchParams
   const animes = await fetchAnimes(params)
+  const uniqueAnimes = deleteDuplicate(animes.data)
 
   return (
     <>
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-items-center-safe gap-3 sm:gap-5'>
-        {animes.data.map((anime) => (
+        {uniqueAnimes.map((anime) => (
           <AnimeCard key={anime.mal_id} anime={anime} />
         ))}
       </div>
