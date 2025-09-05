647fdee2e4df7c641167558c7a4ead23ac51e717
OverCode <avalosfigueroapedro@gmail.com>
2025-09-05 17:13:05 -0500

    feat: añadir componentes de layout y página para mostrar animes en curso

---
+++ app/ongoing/layout.tsx
@@ -0,0 +1,11 @@
+import FormSearch from '@/components/common/Search/FormSearch'
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className='min-h-svh spacing-x pt-24'>
+      <h1 className='mb-3 text-2xl font-bold'>What's On This Season</h1>
+      <FormSearch />
+      {children}
+    </main>
+  )
+}
---
+++ app/ongoing/page.tsx
@@ -0,0 +1,22 @@
+import AnimeCard from '@/components/common/AnimeCard'
+import Pagination from '@/components/common/Search/Pagination'
+import { fetchSeasonNow } from '@/lib/api'
+import { deleteDuplicate } from '@/lib/utils'
+import { AnimeQueryParameters } from '@/types/AnimeQueryParameters'
+
+export default async function page({ searchParams }: { searchParams: Promise<AnimeQueryParameters> }) {
+  const params = await searchParams
+  const animes = await fetchSeasonNow(params)
+  const uniqueAnimes = deleteDuplicate(animes.data)
+
+  return (
+    <>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-items-center-safe gap-3 sm:gap-5'>
+        {uniqueAnimes.map((anime) => (
+          <AnimeCard key={anime.mal_id} anime={anime} />
+        ))}
+      </div>
+      <Pagination paginationData={animes.pagination} />
+    </>
+  )
+}
