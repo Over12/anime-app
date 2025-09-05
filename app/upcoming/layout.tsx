674bd974cfd26ad3f824afbffe37a42765efb3a3
OverCode <avalosfigueroapedro@gmail.com>
2025-09-05 17:19:47 -0500

    feat: añadir componentes de layout y página para mostrar animes próximos

---
+++ app/upcoming/layout.tsx
@@ -0,0 +1,11 @@
+import FormSearch from '@/components/common/Search/FormSearch'
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className='min-h-svh spacing-x pt-24'>
+      <h1 className='mb-3 text-2xl font-bold'>Anime to look forward to</h1>
+      <FormSearch />
+      {children}
+    </main>
+  )
+};
