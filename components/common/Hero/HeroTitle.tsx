6129b1cde09eac1c620fd9b104eb9fc621138697
OverCode <avalosfigueroapedro@gmail.com>
2025-09-26 18:31:18 -0500

    fix: corregir entidades HTML en los títulos para una mejor compatibilidad

--- components/common/Hero/HeroTitle.tsx
+++ components/common/Hero/HeroTitle.tsx
@@ -2,7 +2,7 @@ export default function HeroTitle() {
   return (
     <div className='size-full text-center sm:text-start sm:min-w-1/3'>
       <h1 className='mb-5 text-5xl sm:text-6xl font-bold'>Anime App</h1>
-      <p>Don't know what to watch? Let us help you find the perfect anime!</p>
+      <p>Don&apos;t know what to watch? Let us help you find the perfect anime!</p>
     </div>
   )
 }
