c5accbd9d9628ac45924664a1f80da486c87457f
OverCode <avalosfigueroapedro@gmail.com>
2025-09-26 18:54:49 -0500

    feat: envolver el componente FormSearch en Suspense para mejorar la carga de datos

--- app/ongoing/layout.tsx
+++ app/ongoing/layout.tsx
@@ -1,10 +1,13 @@
 import FormSearch from '@/components/common/Search/FormSearch'
+import { Suspense } from 'react'
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <main className='min-h-svh spacing-x pt-24'>
       <h1 className='mb-3 text-2xl font-bold'>What&apos;s On This Season</h1>
-      <FormSearch />
+      <Suspense>
+        <FormSearch />
+      </Suspense>
       {children}
     </main>
   )
